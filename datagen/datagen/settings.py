f57dc4eca6792a5b653f844bb8768ae6ea5d5df8
Ivan Omelchenko <zver2010-3@ya.ru>
2022-05-07 18:55:03 +0300

    Changed python version

--- datagen/datagen/settings.py
+++ datagen/datagen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*uai@*zrp=zf3=)1um#x)y*%gw_9pi(@z%@4*+!o)^j$x)gtu5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
