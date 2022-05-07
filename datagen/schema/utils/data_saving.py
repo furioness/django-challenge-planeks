d1ebfaaa57911effa48adb610bcb00b17d823693
Ivan Omelchenko <zver2010-3@ya.ru>
2022-05-07 21:09:10 +0300

    Fix csv file saving to nonexistent dir

--- datagen/schema/utils/data_saving.py
+++ datagen/schema/utils/data_saving.py
@@ -1,8 +1,11 @@
 import csv
+from pathlib import Path
+
 from .generator import Schema
 
 
 def generate_to_csv(schema: Schema, num_rows: int, delimiter, quotechar, filename):
+    Path(filename).parent.mkdir(parents=True, exist_ok=True)
     with open(filename, 'w') as csv_file:
         csv_writer = csv.writer(csv_file,
                                 delimiter=delimiter, 
