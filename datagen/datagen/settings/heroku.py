8c11e60333d8b6568c21454b569a876df95a7703
Ivan Omelchenko <zver2010-3@ya.ru>
2022-05-08 12:44:02 +0300

    Set AWS S3 as static storage

--- datagen/datagen/settings/heroku.py
+++ datagen/datagen/settings/heroku.py
@@ -15,6 +15,20 @@ ALLOWED_HOSTS = ['datagen-challenge.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config(default=environ['DATABASE_URL'])
 
+INSTALLED_APPS += ['storages']
+
+AWS_ACCESS_KEY_ID = environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = environ['AWS_STORAGE_BUCKET_NAME']
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# s3 static settings
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 # https://www.cloudamqp.com/docs/celery.html
 CELERY_BROKER = environ['CLOUDAMQP_URL']
 CELERY_BROKER_POOL_LIMIT = 1 # Will decrease connection usage
