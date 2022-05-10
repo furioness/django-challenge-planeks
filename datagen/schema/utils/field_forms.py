91479a655d559d000a8b86707f48b77240448394
Ivan Omelchenko <zver2010-3@ya.ru>
2022-05-10 22:25:15 +0300

    Refactor field_form module to ease adding new fields

--- datagen/schema/utils/field_forms.py
+++ datagen/schema/utils/field_forms.py
@@ -4,36 +4,34 @@ from .generator import Field
 
 
 class BaseFieldForm(forms.Form):
-    label = ''
-    type = ''
-    f_params = ()
+    label: str
+    type: str
+    f_params: tuple
     
     f_type = forms.CharField(initial='',  widget=forms.HiddenInput())
     name = forms.CharField(label='Column name', max_length=100, required=True)
     order = forms.IntegerField(label='Order', min_value=0, required=True)
     
-    # def __new__(cls, *args, **kwargs):
-    #     res = object.__new__(cls)
-    #     if not res.f_type:
-    #         raise NotImplementedError('f_type is not set')
-    #     res.base_fields['type'].initial = cls.f_type
-    #     return res
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, 'type'):
+            raise NotImplementedError('type is not set')
+        if not hasattr(cls, 'label'):
+            cls.label = cls.type.title()
+        if not hasattr(cls, 'f_params'):
+            #  look for base fields as Field metaclass moves from class own
+            cls.f_params = tuple(field for field in cls.base_fields 
+                            if field not in ('f_type', 'name', 'order'))
+        cls.base_fields['f_type'].initial = cls.type
+        return object.__new__(cls)
     
     def __init__(self, data=None, *args, **kwargs):
-        if not self.type:
-            raise NotImplementedError('type is not set')
-        if not self.label:
-            raise NotImplementedError('label is not set')
-        
-        self.base_fields['f_type'].initial = self.type
         if data:
             params = data.pop('f_params', {})
             data.update(params)
         super().__init__(data, *args, **kwargs)
         
-        
     def to_schema_field(self):
-        return Field(name=self.cleaned_data['name'], 
+            return Field(name=self.cleaned_data['name'], 
                      f_type=self.type, 
                      f_params=self.get_params(), 
                      order=self.cleaned_data['order'])
@@ -43,12 +41,11 @@ class BaseFieldForm(forms.Form):
     
 class FullNameFieldForm(BaseFieldForm):
     type = 'name'
-    label = 'Name'
+    
     
 class RandomIntFieldForm(BaseFieldForm):
     type = 'random_int'
     label = 'Random integer'
-    f_params = ('min', 'max')
     
     min = forms.IntegerField(label='Min', min_value=-9999999, max_value=9999999, required=True)
     max = forms.IntegerField(label='Max', min_value=-9999999, max_value=9999999, required=True)
@@ -64,10 +61,8 @@ class RandomIntFieldForm(BaseFieldForm):
         return cleaned_data
 
     
-FIELD_FORMS = {
-        'name': FullNameFieldForm,
-        'random_int': RandomIntFieldForm
-    }
+FIELD_FORMS = {field_form.type:field_form 
+               for field_form in BaseFieldForm.__subclasses__()}
  
 def get_form_for_field(field: Field):
     return FIELD_FORMS[field.f_type](data={
