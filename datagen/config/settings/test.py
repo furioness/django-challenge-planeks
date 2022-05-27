6987760224ce822b0c07dd785c45d1a25f96622d
Ivan Omelchenko <zver2010-3@ya.ru>
2022-05-27 15:54:52 +0300

    Fix incorrect RabbitMQ testing vhost

--- datagen/config/settings/test.py
+++ datagen/config/settings/test.py
@@ -2,4 +2,4 @@ from .local_docker import *
 
 # Using the same database  for tests as Django creates separate temporal testing database
 
-CELERY_BROKER = "pyamqp://@localhost/testing/"
+CELERY_BROKER = "pyamqp://@localhost/testing"
