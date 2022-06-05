54d2fbe516b22d971ec613f4938f8a027fca3cc2
Ivan Omelchenko <zver2010-3@ya.ru>
2022-06-05 19:41:17 +0300

    Enable templates debug option for coverage plugin working

--- datagen/config/settings/test.py
+++ datagen/config/settings/test.py
@@ -3,3 +3,5 @@ from .local_docker import *
 # Using the same database  for tests as Django creates separate temporal testing database
 
 CELERY_BROKER = "pyamqp://@localhost/testing"
+
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # For templates coverage plugin
