d4d36b5ec35782a77297fe37b15d7dd9305f25b3
Ivan Omelchenko <zver2010-3@ya.ru>
2022-06-18 22:48:18 +0300

    Adjust schema_edit_view
    Signed-off-by: Ivan Omelchenko <zver2010-3@ya.ru>

--- datagen/schema/tests/test_views/test_schema_edit_view.py
+++ datagen/schema/tests/test_views/test_schema_edit_view.py
@@ -3,45 +3,37 @@ from django.urls import resolve, reverse
 from django.contrib.auth import get_user_model
 
 from ... import views
-from ...models import Schema
+from ...models import BaseColumn, NameColumn, RandomIntColumn, Schema
 
 
 class TestEditSchemaView(TestCase):
+    """Test just edit specific problems.
+    Other edge cases like data validation and error renering
+    are covered by schema_create and form tests."""
+
     @classmethod
     def setUpTestData(cls):
         cls.user = get_user_model().objects.create_user(  # type: ignore
             username="testuser", password="12345"
         )
         cls.schema = Schema.objects.create(
-            **{
-                "name": "Test schema",
-                "column_separator": ",",
-                "quotechar": '"',
-                "fields": [
-                    {
-                        "name": "Name",
-                        "order": 1,
-                        "f_type": "name",
-                        "f_params": {},
-                    },
-                    {
-                        "name": "Age",
-                        "order": 2,
-                        "f_type": "random_int",
-                        "f_params": {"min": 5, "max": 67},
-                    },
-                ],
-            },
+            name="Test schema",
+            column_separator=",",
+            quotechar='"',
             user=cls.user,
+        )
+        NameColumn.objects.create(name="Name", order=1, schema=cls.schema)
+        RandomIntColumn.objects.create(
+            name="Age", order=2, max=55, min=5, schema=cls.schema
         )
 
     @property
     def VIEW_URL(self):
-        return reverse("schema:update", kwargs={"pk": self.schema.pk})
+        return reverse("schema:edit", kwargs={"pk": self.schema.pk})
 
     def test_url_resolves_to_view(self):
         self.assertIs(
-            resolve(self.VIEW_URL).func.view_class, views.UpdateSchemaView
+            resolve(self.VIEW_URL).func.view_class, views.EditSchemaView
         )
 
     def test_call_view_deny_anonymous(self):
@@ -64,7 +56,7 @@ class TestEditSchemaView(TestCase):
     def test_shows_correct_schema(self):
         self.client.force_login(self.user)
         response = self.client.get(self.VIEW_URL)
-        self.assertEqual(response.context["schema"], self.schema)
+        self.assertEqual(response.context["schema"], self.schema)  # type: ignore
 
     def test_shows_only_to_owner(self):
         user_2 = get_user_model().objects.create_user(  # type: ignore
@@ -75,7 +67,7 @@ class TestEditSchemaView(TestCase):
         self.assertEqual(response.status_code, 404)
 
     def test_returns_404_if_schema_does_not_exist(self):
-        url = reverse("schema:update", kwargs={"pk": self.schema.pk + 1})
+        url = reverse("schema:edit", kwargs={"pk": self.schema.pk + 1})
         self.client.force_login(self.user)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
