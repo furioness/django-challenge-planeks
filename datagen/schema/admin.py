db8a9bfed7b8be037479d7fcd27ec3c77791a96b
Ivan Omelchenko <zver2010-3@ya.ru>
2022-06-19 13:09:00 +0300

    Add some fairly usable column admin views
    Signed-off-by: Ivan Omelchenko <zver2010-3@ya.ru>

--- datagen/schema/admin.py
+++ datagen/schema/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 
-from .models import Dataset, Schema, NameColumn, RandomIntColumn
+from .models import BaseColumn, Dataset, Schema
 
 
-admin.site.register(Dataset)
+admin.site.register(
+    Dataset, list_display=("id", "schema", "num_rows", "created", "file")
+)
 
-admin.site.register(NameColumn)
-admin.site.register(RandomIntColumn)
 
-
-class NameColumnInline(admin.TabularInline):
-    model = NameColumn
-    list_display = ("name",)
+def column_inline_factory(model):
+    return type(
+        "whatever", (admin.TabularInline,), {"model": model, "extra": 0}
+    )
 
 
 class SchemaAdmin(admin.ModelAdmin):
-    inlines = [NameColumnInline]
+    list_display = ("id", "name", "modified", "user")
+    inlines = [
+        column_inline_factory(column) for column in BaseColumn.__subclasses__()
+    ]
 
 
 admin.site.register(Schema, SchemaAdmin)
