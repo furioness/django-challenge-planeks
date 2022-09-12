9362e07b5460388692e1628a682b487f3190fb39
Ivan Omelchenko <zver2010-3@ya.ru>
2022-09-12 20:58:02 +0300

    Completing existing serializers test
    Made to work with order field and various TODO parts fixed.
    
    Signed-off-by: Ivan Omelchenko <zver2010-3@ya.ru>

--- datagen/schema/tests/test_rest_api/test_serializers.py
+++ datagen/schema/tests/test_rest_api/test_serializers.py
@@ -1,9 +1,11 @@
+import unittest
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.forms.models import model_to_dict
 
 from ...rest_api.serializers import SchemaSerializer
-from ...models import NameColumn, RandomIntColumn
+from ...models import NameColumn, RandomIntColumn, Schema
 
 
 class TestSchemaSerializer(TestCase):
@@ -54,15 +56,21 @@ class TestSchemaSerializer(TestCase):
             "columns": [
                 {
                     "type": "non_existent_type",
-                    "params": {"name": "Full name"},
+                    "params": {"name": "Full name", "order": 1},
                 },
             ],
         }
 
         serializer = SchemaSerializer(data=schema_data)
         self.assertFalse(serializer.is_valid())
-        # TODO: add an assertion for specific error messages
 
+        column_errors = serializer.errors["columns"][0]
+        self.assertEqual(len(column_errors), 1)
+        self.assertTrue("type" in column_errors)
+
+    @unittest.skip(
+        reason="Seems like it's the default behavior for DRF to allow superfluous fields."
+    )
     def test_serialize_nonexistent_col_param(self):
         schema_data = {
             "name": "People",
@@ -71,16 +79,19 @@ class TestSchemaSerializer(TestCase):
             "columns": [
                 {
                     "type": "random_int",
-                    "params": {"name": "Full name", "nonexistent_param": 1337},
+                    "params": {
+                        "name": "Full name",
+                        "order": 1,
+                        "nonexistent_param": 1337,
+                    },
                 },
             ],
         }
 
         serializer = SchemaSerializer(data=schema_data)
         self.assertFalse(serializer.is_valid())
-        # TODO: add an assertion for specific error messages
 
-    def test_serialize_invalid_col_param(self):
+    def test_serialize_invalid_col_param_value(self):
         schema_data = {
             "name": "People",
             "column_separator": ";",
@@ -88,13 +99,21 @@ class TestSchemaSerializer(TestCase):
             "columns": [
                 {
                     "type": "random_int",
-                    "params": {"name": "Full name", "min": 1337, "max": 322},
+                    "params": {
+                        "name": "Full name",
+                        "order": 1,
+                        "min": 1337,
+                        "max": 322,
+                    },
                 },
             ],
         }
         serializer = SchemaSerializer(data=schema_data)
         self.assertFalse(serializer.is_valid())
-        # TODO: add an assertion for specific error messages
+
+        column_errors = serializer.errors["columns"][0]
+        self.assertEqual(len(column_errors), 3)
+        self.assertEqual(set(["__all__", "min", "max"]), set(column_errors))
 
     def test_serializer_create_from_validated(self):
         schema_data = {
@@ -115,5 +134,15 @@ class TestSchemaSerializer(TestCase):
         serializer = SchemaSerializer(data=schema_data)
         self.assertTrue(serializer.is_valid(), serializer.errors)
 
-        schema = serializer.save(user=self.user)
-        print(schema, *schema.columns)
+        serializer.save(user=self.user)
+
+        schema_from_db = Schema.objects.first()
+        own_schema_fields = model_to_dict(
+            schema_from_db, fields=["name", "column_separator", "quotechar"]
+        )
+        self.assertEqual(schema_data, schema_data | own_schema_fields)
+
+        columns = list(schema_from_db.columns)
+        self.assertEqual(len(columns), 2)
+        for column in columns:
+            self.assertTrue(column.name in ["Full name", "Age"])
